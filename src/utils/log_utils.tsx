fc0aec33fd2148d1f930aa835e505c069e0db20f
bippolaroid <bippolaroid@proton.me>
2025-06-03 10:47:49 -0400

    fixed time formatting for new proj

--- src/utils/log_utils.tsx
+++ src/utils/log_utils.tsx
@@ -1,5 +1,15 @@
-export function UTCNow() {
+export function UTCNow(): string {
     const date = new Date();
-    let utcDate = `${date.getUTCFullYear()}-${date.getUTCMonth()}-${date.getUTCDay()} ${date.getUTCHours()}:${date.getUTCMinutes()}:${date.getUTCSeconds()}.${date.getUTCMilliseconds()} UTC`
-    return utcDate;
+
+    const pad = (n: number) => n.toString().padStart(2, '0');
+
+    const year = date.getUTCFullYear();
+    const month = pad(date.getUTCMonth() + 1);
+    const day = pad(date.getUTCDate());
+    const hours = pad(date.getUTCHours());
+    const minutes = pad(date.getUTCMinutes());
+    const seconds = pad(date.getUTCSeconds());
+    const milliseconds = date.getUTCMilliseconds().toString().padStart(3, '0');
+
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}.${milliseconds} UTC`;
 }
